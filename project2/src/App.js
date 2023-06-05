09ca8aebf2671304429ab6c97ef113bd91e641f4
Vikas Sharma <vkassharma19492@gmail.com>
2023-06-06 01:35:05 +0530

    creating notes full stack app for testing purpose

--- project2/src/App.js
+++ project2/src/App.js
@@ -1,5 +1,21 @@
 
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const baseUrl = 'http://localhost:3001/notes'
+
 const App = () => {
+
+  const [ notes, setNotes ] = useState([])
+
+  const hook = () => {
+    axios.get(baseUrl).then(response => {
+      console.log(response.data)
+      setNotes(response.data)
+    })
+  }
+  useEffect(hook, [])
+
   return (
     <div>
       <h1>Hello World</h1>
