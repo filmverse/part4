4c2c12122834f58a18b4e795f1726ce852ffb8e8
Vikas Sharma <vkassharma19492@gmail.com>
2023-06-07 01:49:05 +0530

    creating notes app for testing purpose

--- project2/src/App.js
+++ project2/src/App.js
@@ -2,6 +2,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Note from './components/Note';
+import NoteForm from './components/NoteForm';
 
 const baseUrl = 'http://localhost:3001/notes'
 
@@ -40,10 +41,7 @@ const App = () => {
     <div>
       <h2>Notes</h2>
       {notes.map(note => <Note key={note.id} note={note} />)}
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleChange(setNewNote)}/>
-        <button type="submit">save</button>
-      </form>
+      <NoteForm addNote={addNote} newNote={newNote} handleChange={handleChange(setNewNote)} />
       <p>Debug: {newNote}</p>
     </div>
   )
---
+++ project2/src/components/NoteForm.js
@@ -0,0 +1,13 @@
+
+const NoteForm = ({ addNote, newNote, handleChange }) => {
+    return (
+        <div>
+            <form onSubmit={addNote}>
+                <input value={newNote} onChange={handleChange} />
+                <button type="submit">save</button>
+            </form>
+        </div>
+    )
+}
+
+export default NoteForm;
