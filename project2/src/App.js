7020157913ddc7ce4b70a341151512262e0f0841
Vikas Sharma <vkassharma19492@gmail.com>
2023-06-07 01:39:46 +0530

    creating notes app for testing purpose

--- project2/src/App.js
+++ project2/src/App.js
@@ -8,6 +8,7 @@ const baseUrl = 'http://localhost:3001/notes'
 const App = () => {
 
   const [ notes, setNotes ] = useState([])
+  const [ newNote, setNewNote ] = useState("")
 
   const hook = () => {
     axios.get(baseUrl).then(response => {
@@ -17,10 +18,33 @@ const App = () => {
   }
   useEffect(hook, [])
 
+  const addNote = (event) => {
+    event.preventDefault()
+    const addNote = {
+      content: newNote,
+      important: Math.random() < 0.5,
+    }
+    axios.post(baseUrl, addNote).then(
+      response => {
+        setNotes(notes.concat(response.data))
+        setNewNote("")
+      }
+    )
+  }
+
+  const handleChange = (funObj) => (event) => {
+    funObj(event.target.value)
+  }
+
   return (
     <div>
       <h2>Notes</h2>
       {notes.map(note => <Note key={note.id} note={note} />)}
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleChange(setNewNote)}/>
+        <button type="submit">add</button>
+      </form>
+      <p>Debug: {newNote}</p>
     </div>
   )
 }
