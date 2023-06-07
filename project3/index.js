05c5290ecac726c6db99d06bc30bb681135e00ef
Vikas Sharma <vkassharma19492@gmail.com>
2023-06-08 02:24:17 +0530

    creating notes app backend for testing purpose

--- project3/index.js
+++ project3/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 let notes = [
     {
@@ -41,6 +42,30 @@ app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
     notes = notes.filter(note => note.id !== id)
     response.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map(note => note.id))
+        : 0
+    return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+    if (!body.content) {
+        response.status(400).json({
+            error: "content missing"
+        })
+    } else {
+        const note = {
+            content: body.content,
+            important: body.important || false,
+            id: generateId(),
+        }
+        notes = notes.concat(note)
+        response.json(note)
+    }
 })
 
 
