c9fc33826ec8aa9eef8113a4a9cfff8d07acff2b
Vikas Sharma <vkassharma19492@gmail.com>
2023-06-14 02:33:21 +0530

    connecting mongodb database using mongoose

--- project3/index.js
+++ project3/index.js
@@ -19,7 +19,7 @@ app.get('/api/notes', (request, response) => {
     })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id)
         .then(note => {
             if (note) {
@@ -28,10 +28,7 @@ app.get('/api/notes/:id', (request, response) => {
                 response.status(404).end()
             }
         })
-        .catch(error => {
-            console.log(error)
-            response.status(400).send({ error: 'malformated id' })
-        })
+        .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -73,6 +70,15 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformated id' })
+    }
+}
+
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT || 3001
