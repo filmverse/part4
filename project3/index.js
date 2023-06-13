66ec8e62a80aa1736618d8194f50566d5ec47ffd
Vikas Sharma <vkassharma19492@gmail.com>
2023-06-14 02:49:54 +0530

    connecting mongodb database using mongoose

--- project3/index.js
+++ project3/index.js
@@ -67,6 +67,19 @@ app.post('/api/notes', (request, response) => {
     }
 })
 
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
+    Note.findByIdAndUpdate(request.params.id, note, {new: true})
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
+})
+
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
