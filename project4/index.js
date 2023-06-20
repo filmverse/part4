e8e8fab3b3e063383442cf15ed41e8af3839c0b0
VIKAS SHARMA <vkassharma19492@gmail.com>
2023-06-20 19:34:26 +0530

    structuring backend (exercise 4.1&4.2) and introduction to testing

--- project4/index.js
+++ project4/index.js
@@ -2,20 +2,9 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
+const Blog = require('./models/blog')
 const info = require('./utils/logger')
 
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
